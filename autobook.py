28461fe9e7a9f15198089277904c84f107e11f49
Thomas Liu <thomasliu02@gmail.com>
2015-01-31 18:25:32 -0600

    Changed input to raw_input and got rid of duplicate checking. Resolves #7

--- autobook.py
+++ autobook.py
@@ -138,16 +138,16 @@ def reorder_book(book):
 	print "This is the current ordering of the book"
 	for ii in range(len(book)):
 		chapter = book[ii]
-		print "\t Chapter "+str(ii)+":"+chapter.chapter_name
+		print "\t Chapter "+str(ii)+": "+chapter.chapter_name
 	order = []
 	while True:
-		ordering = input("Please provide the ordering of the chapters as a comma seperated list (ex. 3,1,2)")
+		ordering = raw_input("Please provide the ordering of the chapters as a comma seperated list (ex. 3,1,2): ")
 		try:
 			order  = [int(position) for position in ordering.split(",")]
 		except ValueError:
 			print "Error in input!!!!"
 			continue
-		order = list(set(order))
+		
 		if len(order) == len(book):
 			break
 		print "You have not specified the ordering for all chapters!!!"
